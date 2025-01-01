d8ee8a022b506fcdba26357d12ee9532c9a810b6
Marwin2002 <musicbotid@gmail.com>
2025-01-01 19:48:19 +0530

    Update mongo.py

--- VIPMUSIC/core/mongo.py
+++ VIPMUSIC/core/mongo.py
@@ -30,7 +30,7 @@ if config.MONGO_DB_URI is None:
     mongodb = _mongo_async_[username]
     pymongodb = _mongo_sync_[username]
 else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+   'MongoClient' = _mongo_client_(config.MONGO_DB_URI)
+   'MongoClient' = MongoClient(config.MONGO_DB_URI)
     mongodb = _mongo_async_[DB_NAME]
     pymongodb = _mongo_sync_[DB_NAME]
